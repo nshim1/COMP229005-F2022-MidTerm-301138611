6ae0fb7ccc14542d9a7eff30996019bc6543cce3
Nayeon Shim <nshim1@my.centennialcollege.ca>
2022-10-30 20:52:17 -0400

    Personal information

--- public/javascripts/appclient.js
+++ public/javascripts/appclient.js
@@ -1,39 +1,36 @@
 // Filename: appclient.js
-// Student: Julio Vinicius
-// StudentId: 123456789
-// Date: Jun 02, 2022
+// Student: Nayeon shim
+// StudentId: 301138611
+// Date: Oct 29, 2022
+//Web App name: To-do list
 
-console.log('app script is working.');
+console.log("app script is working.");
 
-if(getTitle == "To-Do List")
-{
-    let deleteButtons = document.querySelectorAll('.btn-danger');
-        
-    for(button of deleteButtons)
-    {
-        button.addEventListener('click', (event)=>{
-            if(!confirm("Are you sure?")) 
-            {
-                event.preventDefault();
-            }
-        });
-    }
+if (getTitle == "To-Do List") {
+  let deleteButtons = document.querySelectorAll(".btn-danger");
+
+  for (button of deleteButtons) {
+    button.addEventListener("click", (event) => {
+      if (!confirm("Are you sure?")) {
+        event.preventDefault();
+      }
+    });
+  }
 }
 
-if(getTitle == "Sign-up Form")
-{
-    const confirm = document.querySelector('input[name=password_confirm]');
+if (getTitle == "Sign-up Form") {
+  const confirm = document.querySelector("input[name=password_confirm]");
 
-    confirm.addEventListener('change', onChange); 
+  confirm.addEventListener("change", onChange);
 }
 
 function onChange() {
-    const password = document.querySelector('input[name=password]');
-    const confirm = document.querySelector('input[name=password_confirm]');
-    
-    if (confirm.value === password.value) {
-      confirm.setCustomValidity('');
-    } else {
-      confirm.setCustomValidity('Passwords do not match');
-    }
+  const password = document.querySelector("input[name=password]");
+  const confirm = document.querySelector("input[name=password_confirm]");
+
+  if (confirm.value === password.value) {
+    confirm.setCustomValidity("");
+  } else {
+    confirm.setCustomValidity("Passwords do not match");
+  }
 }
